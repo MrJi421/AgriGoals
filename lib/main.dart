da92dbfe94cdbdcb76c5fb0f4d48d9fd04dfd675
Hemant Kumar <mr.ji421@outlook.com>
2024-02-11 23:01:23 +0530

    login and signup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 // import 'package:flutter_icons/flutter_icons.dart'; // Import flutter_icons package
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
+// import 'package:flutter/material.dart';
+// Import statements...
 
 void main() {
   runApp(const MyApp());
@@ -38,9 +39,9 @@ class LoginPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Image(
-                  image: AssetImage('lib/assets/logo.png'),
-                  height: 200,
-                ),
+              image: AssetImage('lib/assets/logo.png'),
+              height: 200,
+            ),
             const SizedBox(height: 20),
             const Text(
               'Sign In',
@@ -52,56 +53,35 @@ class LoginPage extends StatelessWidget {
             const SizedBox(height: 20),
             TextFormField(
               decoration: const InputDecoration(
-                labelText: 'Email',
+                labelText: 'Phone No.',
               ),
             ),
             const SizedBox(height: 10),
             TextFormField(
               obscureText: true,
               decoration: const InputDecoration(
-                labelText: 'Password',
+                labelText: 'OTP',
               ),
             ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    // Handle forgot password
-                  },
-                  child: const Text(
-                    'Forgot Password?',
-                    style: TextStyle(
-                      color: Colors.blue,
-                    ),
-                  ),
+
+            
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Handle sign in button press
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFF613EEA),
+                fixedSize: const Size(200, 50),
+              ),
+              child: const Text(
+                'Sign In',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
                 ),
-              ],
+              ),
             ),
-           const SizedBox(height: 20),
-ElevatedButton(
-  onPressed: () {
-    // Handle sign in button press
-
-
-
-
-
-  },
-  style: ElevatedButton.styleFrom(
-    backgroundColor: const Color(0xFF613EEA),
-    fixedSize: Size(200, 50),// Use backgroundColor instead of primary
-  ),
-  child: const Text(
-    'Sign In',
-    style: TextStyle(
-      color: Colors.white, // Set text color to white
-      fontSize: 25, // Set text size to 18
-  ),
-),
-),
-
             const SizedBox(height: 20),
             const Text(
               'Or sign in with',
@@ -111,27 +91,59 @@ ElevatedButton(
             ),
             const SizedBox(height: 10),
             Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 IconButton(
                   onPressed: () {
                     // Handle sign in with Google
                   },
-                  icon: const Icon(FontAwesomeIcons.google), // Use FontAwesome.google for Google icon
+                  icon: const Icon(FontAwesomeIcons.google),
                 ),
                 IconButton(
                   onPressed: () {
-                    // Handle sign in with Google
+                    // Handle sign in with Facebook
                   },
-                  icon: const Icon(FontAwesomeIcons.facebook), // Use FontAwesome.google for Google icon
+                  icon: const Icon(FontAwesomeIcons.facebook),
                 ),
                 IconButton(
                   onPressed: () {
                     // Handle sign in with Twitter
                   },
-                  icon: const Icon(FontAwesomeIcons.twitter), // Use FontAwesome.twitter for Twitter icon
+                  icon: const Icon(FontAwesomeIcons.twitter),
                 ),
               ],
+            ),
+            const SizedBox(height: 20),
+            Center(
+              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'New to AgriGoals? ',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      // Navigate to the signup page
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const SignupPage(),
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      'Sign Up',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
@@ -142,6 +154,138 @@ ElevatedButton(
 
 
 
-class SignupPage extens StatelessWidget{
-  
+
+//signup page
+
+class SignupPage extends StatelessWidget {
+  const SignupPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('AgriGoals'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Image(
+              image: AssetImage('lib/assets/logo.png'),
+              height: 200,
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Sign Up',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Name',
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Phone No.',
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: 'OTP',
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Handle sign up button press
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFF613EEA),
+                fixedSize: const Size(200, 50),
+              ),
+              child: const Text(
+                'Sign Up',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Or sign up with',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    // Handle sign up with Google
+                  },
+                  icon: const Icon(FontAwesomeIcons.google),
+                ),
+                IconButton(
+                  onPressed: () {
+                    // Handle sign up with Facebook
+                  },
+                  icon: const Icon(FontAwesomeIcons.facebook),
+                ),
+                IconButton(
+                  onPressed: () {
+                    // Handle sign up with Twitter
+                  },
+                  icon: const Icon(FontAwesomeIcons.twitter),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Already signed up? ',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      // Navigate to the signin page
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const LoginPage(),
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      'Sign In',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
